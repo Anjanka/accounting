6e3e9f2a7ad2eb9bb943b53335004cdc6fdcff44
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2018-01-04 19:48:13 +0100

    #3: Fix naming problem

---
+++ src/test/scala/util/CompositionTest.scala
@@ -0,0 +1,16 @@
+package util
+
+import org.scalacheck.{Prop, Properties}
+
+import util.Composition.comp
+
+class CompositionTest extends Properties("Composition properties") {
+
+  property("translations commute") = Prop.forAll { (t: Long, s: Long) =>
+    val first = t + (_: Long)
+    val second = s + (_: Long)
+
+    Prop.forAll((n: Long) => comp(first, second)(n) == comp(second, first)(n))
+  }
+
+}
