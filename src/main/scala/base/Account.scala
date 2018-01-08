6dd7ea403401c8fc5d8c5e82db78127547e2abc8
Anja Hildebrandt <tovija@gmx.de>
2018-01-08 18:37:02 +0100

    #5 implement missing balance method

--- src/main/scala/base/Account.scala
+++ src/main/scala/base/Account.scala
@@ -22,6 +22,22 @@ trait Account {
 
 object Account {
 
-  private def balance(account: Account, entries: List[AEntry]): Balance = ???
+
+  private def balance(account: Account, entries: List[AEntry]): Balance = {
+    /* takes an Account returns a collection of Accounting entries where the
+    credit or debit matches the given Account
+     */
+    def byAccount(cd: AEntry => Account): List[AEntry] =
+      entries.filter(entry => cd(entry).id == account.id)
+    val credit: List[AEntry] = byAccount(_.credit)
+    val debit: List[AEntry] = byAccount(_.debit)
+
+    // takes a collection of Accounting Entries and returns the sum of the amounts
+    def makeSum (es: List[AEntry]): MonetaryValue = es.map(_.amount).sum
+    val creditSum = makeSum(credit)
+    val debitSum = makeSum(debit)
+
+    creditSum - debitSum
+  }
 
 }
