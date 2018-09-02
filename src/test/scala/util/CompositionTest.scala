f69b308d0bda576a45e553b07932c83e80a57c90
Anja Hildebrandt <tovija@gmx.net>
2018-09-02 15:29:43 +0200

    Remove test commit.

--- src/test/scala/util/CompositionTest.scala
+++ src/test/scala/util/CompositionTest.scala
@@ -7,8 +7,8 @@ import util.Composition.comp
 object CompositionTest extends Properties("Composition properties") {
 
   property("translations commute") = Prop.forAll { (t: Long, s: Long) =>
-    val second = s + (_: Long)
     val first = t + (_: Long)
+    val second = s + (_: Long)
 
     Prop.forAll((n: Long) => comp(first, second)(n) == comp(second, first)(n))
   }
