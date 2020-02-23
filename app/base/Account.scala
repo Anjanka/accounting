17ff145e592508019389a2c84c3d7f8532c6d287
Anja Hildebrandt <tovija@gmx.de>
2020-02-23 19:37:20 +0100

    Simplify account to a single case class

--- app/base/Account.scala
+++ app/base/Account.scala
@@ -2,19 +2,13 @@ package base
 
 import scala.Numeric.Implicits._
 
-trait Account {
+/**
+  * @param id    The unique number associated with this account.
+  * @param title The unique name associated with this account.
+  */
+case class Account(id: Int, title: String) {
   /**
-    * @return The unique number associated with this account.
-    */
-  def id: Int
-
-  /**
-    * @return The unique name associated with this account.
-    */
-  def title: String
-
-  /**
-    * Compute the balance of the account after collection of accounting entries.
+    * Computes the balance of the account after collection of accounting entries.
     *
     * @param entries Collection of '''ALL''' current accounting entries.
     * @return The current (w.r.t. the given entries) balance of this account.
@@ -24,18 +18,19 @@ trait Account {
 
 object Account {
 
-
   private def balance(account: Account, entries: List[AccountingEntry]): Balance = {
     /* takes an Account returns a collection of Accounting entries where the
     credit or debit matches the given Account
      */
     def byAccount(cd: AccountingEntry => Account): List[AccountingEntry] =
       entries.filter(entry => cd(entry).id == account.id)
+
     val credit: List[AccountingEntry] = byAccount(_.credit)
     val debit: List[AccountingEntry] = byAccount(_.debit)
 
     // takes a collection of Accounting Entries and returns the sum of the amounts
-    def makeSum (es: List[AccountingEntry]): MonetaryValue = es.map(_.amount).sum
+    def makeSum(es: List[AccountingEntry]): MonetaryValue = es.map(_.amount).sum
+
     val creditSum = makeSum(credit)
     val debitSum = makeSum(debit)
 
--- app/base/AccountImpl.scala
+++
@@ -1,6 +0,0 @@
-package base
-
-/**
-  * Created by Angel on 05.01.2018.
-  */
-case class AccountImpl(override val id : Int, override val title : String) extends Account{}
