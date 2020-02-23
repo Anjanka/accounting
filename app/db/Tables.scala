8b3a374af6385f8815b14caa89e44b669db24acc
Anja Hildebrandt <tovija@gmx.de>
2020-02-23 19:37:58 +0100

    Begin adding table bindings

---
+++ app/db/Tables.scala
@@ -0,0 +1,17 @@
+package db
+
+import base.Account
+import slick.lifted.{ProvenShape, Tag}
+import slick.lifted.Shape._
+import slick.jdbc.PostgresProfile.api._
+
+object Tables {
+
+  class AccountDB(tag: Tag) extends Table[Account](_tableTag = tag, _tableName = "account") {
+    def id: Rep[Int] = column[Int]("id", O.PrimaryKey)
+    def title: Rep[String] = column[String]("title")
+
+    override def * : ProvenShape[Account] = (id, title) <> ({ case (i, t) => Account(i, t)}, Account.unapply)
+  }
+
+}
