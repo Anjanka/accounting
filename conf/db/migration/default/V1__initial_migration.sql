cbb435a40790fee3c1f3b21e1e1d58d3e339b5f0
Anja Hildebrandt <tovija@gmx.de>
2020-02-23 18:33:17 +0100

    create tables

--- conf/db/migration/default/V1__initial_migration.sql
+++ conf/db/migration/default/V1__initial_migration.sql
@@ -1,5 +1,29 @@
-create table person (
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  middle_name TEXT
+create table account (
+ id integer NOT NULL PRIMARY KEY,
+ title text NOT NULL
+);
+
+
+create table accounting_entry (
+ order_id integer NOT NULL,
+ accounting_year integer NOT NULL,
+
+ booking_date date NOT NULL,
+ receipt_number text NOT NULL,
+ description text NOT NULL,
+ credit integer NOT NULL REFERENCES account(id),
+ debit integer NOT NULL REFERENCES account(id),
+ amount_whole integer NOT NULL,
+ amount_change integer NOT NULL,
+
+ PRIMARY KEY (order_id, accounting_year)
+);
+
+
+create table accounting_entry_template (
+ description text NOT NULL PRIMARY KEY,
+ credit integer NOT NULL REFERENCES account(id),
+ debit integer NOT NULL REFERENCES account(id),
+ amount_whole integer,
+ amount_change integer,
 );
