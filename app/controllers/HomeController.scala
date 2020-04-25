cb0660334a5e32e631ec2d9092b049a42734e552
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-04-25 18:48:37 +0200

    Adjust HomeController for now

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,20 @@
 package controllers
 
+import db.AccountDAO
+import io.circe.syntax._
 import javax.inject._
-import play.api._
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents,
+                               val accountDAO: AccountDAO)(implicit ec: ExecutionContext)
+  extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -20,5 +25,12 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def getAccount(id: Int) = Action.async {
+    accountDAO.findAccount(id).map {
+      x =>
+        Ok(x.asJson.noSpaces)
+    }
   }
 }
