f7fcdc696e47b64b6da507ebbe350e1aced8c80e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-04-25 18:54:52 +0200

    Use SAM

--- app/base/JsonCodecs.scala
+++ app/base/JsonCodecs.scala
@@ -5,10 +5,8 @@ import java.time.LocalDate
 
 import cats.instances.either._
 import cats.syntax.apply._
-import io.circe.Decoder.Result
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder, HCursor, Json}
-
 
 object JsonCodecs {
 
@@ -16,23 +14,19 @@ object JsonCodecs {
     private val dateYear: String = "year"
     private val dateMonth: String = "month"
     private val dateDay: String = "day"
-    implicit val dateDecoder: Decoder[Date] = new Decoder[Date] {
-      override def apply(c: HCursor): Result[Date] = {
-        (c.downField(dateYear).as[Int],
-          c.downField(dateMonth).as[Int],
-          c.downField(dateDay).as[Int]
-          ).mapN((y, m, d) => Date.valueOf(LocalDate.of(y, m, d)))
-      }
+    implicit val dateDecoder: Decoder[Date] = (c: HCursor) => {
+      (c.downField(dateYear).as[Int],
+        c.downField(dateMonth).as[Int],
+        c.downField(dateDay).as[Int]
+      ).mapN((y, m, d) => Date.valueOf(LocalDate.of(y, m, d)))
     }
-    implicit val dateEncoder: Encoder[Date] = new Encoder[Date] {
-      override def apply(a: Date): Json = {
-        val localDate = a.toLocalDate
-        Json.obj(
-          (dateYear, localDate.getYear.asJson),
-          (dateMonth, localDate.getMonthValue.asJson),
-          (dateDay, localDate.getDayOfMonth.asJson)
-        )
-      }
+    implicit val dateEncoder: Encoder[Date] = (a: Date) => {
+      val localDate = a.toLocalDate
+      Json.obj(
+        (dateYear, localDate.getYear.asJson),
+        (dateMonth, localDate.getMonthValue.asJson),
+        (dateDay, localDate.getDayOfMonth.asJson)
+      )
     }
   }
 
