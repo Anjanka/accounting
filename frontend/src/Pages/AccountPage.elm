67604d79ffd0a487dd0f4b32cd664721eba00021
Anja Hildebrandt <tovija@gmx.de>
2020-05-21 17:45:46 +0200

    Create experimental page

---
+++ frontend/src/Pages/AccountPage.elm
@@ -0,0 +1,83 @@
+module Pages.AccountPage exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Http exposing (Error)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions}
+
+
+
+-- MODEL
+
+type alias Model = {
+                      response : String
+                    , feedback : String
+                    , buttonPressed : Bool
+                    }
+type alias Flags = ()
+
+init : Flags -> (Model, Cmd Msg)
+init _ = ({response = "", feedback = "", buttonPressed = False}, Cmd.none)
+
+
+
+-- UPDATE
+
+type Msg = GetAccounts | GotResponse (Result Error String)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GetAccounts ->
+      ({model | buttonPressed = True}, getAccounts)
+
+    GotResponse result ->
+        case result of
+            Ok value ->
+                ({model | response = value, feedback = ""}, Cmd.none)
+
+
+            Err error ->
+                 ({model | feedback = Debug.toString error}, Cmd.none )
+
+
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    let responseArea = if model.buttonPressed then
+                                              [ div [] [text ("feedback : "++ model.feedback)]
+                                              , div [] [text (model.response)]
+                                              ]
+                       else []
+  in div []
+    ([ button [ onClick GetAccounts ] [ text "Get All Accounts" ]] ++ responseArea
+    )
+
+
+
+-- Communication with backend
+
+getAccounts : Cmd Msg
+getAccounts =
+    Http.get
+        {url = "localhost:9000/account/getAllAccounts"
+        , expect = Http.expectString GotResponse}
