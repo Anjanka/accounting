712858c4a7fdc4cb50f415939eb4bb058717fc14
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-05-23 19:48:37 +0200

    Delegate JSON codec to simplified date type codec

--- app/base/JsonCodecs.scala
+++ app/base/JsonCodecs.scala
@@ -3,29 +3,31 @@ package base
 import java.sql.Date
 import java.time.LocalDate
 
-import cats.instances.either._
-import cats.syntax.apply._
-import io.circe.syntax._
-import io.circe.{Decoder, Encoder, HCursor, Json}
+import elm.{Date => ElmDate}
+import io.circe.{Decoder, Encoder}
 
 object JsonCodecs {
 
   object Implicits {
-    private val dateYear: String = "year"
-    private val dateMonth: String = "month"
-    private val dateDay: String = "day"
-    implicit val dateDecoder: Decoder[Date] = (c: HCursor) => {
-      (c.downField(dateYear).as[Int],
-        c.downField(dateMonth).as[Int],
-        c.downField(dateDay).as[Int]
-      ).mapN((y, m, d) => Date.valueOf(LocalDate.of(y, m, d)))
+    implicit val dateDecoder: Decoder[Date] = Decoder[ElmDate].map(dateToSqlDate)
+    implicit val dateEncoder: Encoder[Date] = Encoder[ElmDate].contramap(sqlDateToDate)
+
+    private def sqlDateToDate(date: Date): ElmDate = {
+      val localDate = date.toLocalDate
+      ElmDate(
+        year = localDate.getYear,
+        month = localDate.getMonthValue,
+        day = localDate.getDayOfMonth
+      )
     }
-    implicit val dateEncoder: Encoder[Date] = (a: Date) => {
-      val localDate = a.toLocalDate
-      Json.obj(
-        (dateYear, localDate.getYear.asJson),
-        (dateMonth, localDate.getMonthValue.asJson),
-        (dateDay, localDate.getDayOfMonth.asJson)
+
+    private def dateToSqlDate(date: ElmDate): Date = {
+      Date.valueOf(
+        LocalDate.of(
+          date.year,
+          date.month,
+          date.day
+        )
       )
     }
   }
