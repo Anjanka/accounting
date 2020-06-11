fb6f01bff65992fe14e7c9cf0931f8f763aa259d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-11 23:28:14 +0200

    Remove JSON codecs for classes that should never be serialised

--- app/abstraction/AccountingEntryTemplate.scala
+++ app/abstraction/AccountingEntryTemplate.scala
@@ -1,9 +1,7 @@
 package abstraction
 
 import base.MonetaryValue
-import io.circe.generic.JsonCodec
 
-@JsonCodec
 case class AccountingEntryTemplate(description: String,
                                    credit: Account, //SOLL
                                    debit: Account, //HABEN
