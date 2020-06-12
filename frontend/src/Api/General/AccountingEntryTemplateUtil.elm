19a53486db80a037493ea638d8b73859174aac72
Anja Hildebrandt <tovija@gmx.net>
2020-06-12 08:53:31 +0200

    adjust to type changes in backend

---
+++ frontend/src/Api/General/AccountingEntryTemplateUtil.elm
@@ -0,0 +1,33 @@
+module Api.General.AccountingEntryTemplateUtil exposing (..)
+
+
+import Api.Types.AccountingEntryTemplate exposing (AccountingEntryTemplate)
+
+
+empty : AccountingEntryTemplate
+empty =
+    { description = ""
+    , credit = 0
+    , debit = 0
+    , amountWhole = 0
+    , amountChange = 0
+    }
+
+updateDescription : AccountingEntryTemplate -> String -> AccountingEntryTemplate
+updateDescription aet description = { aet | description = description }
+
+updateCredit : AccountingEntryTemplate -> Int -> AccountingEntryTemplate
+updateCredit aet credit = { aet | credit = credit }
+
+updateDebit : AccountingEntryTemplate -> Int -> AccountingEntryTemplate
+updateDebit aet debit = { aet | debit = debit }
+
+updateAmountWhole : AccountingEntryTemplate -> Int -> AccountingEntryTemplate
+updateAmountWhole aet whole = { aet | amountWhole = whole }
+
+updateAmountChange : AccountingEntryTemplate -> Int -> AccountingEntryTemplate
+updateAmountChange aet change = { aet | amountChange = change }
+
+show: AccountingEntryTemplate -> String
+show aet =
+    String.concat [aet.description, ": ", String.fromInt aet.credit, " - ", String.fromInt aet.debit, ", ", String.fromInt aet.amountWhole, ",", String.fromInt aet.amountChange  ]
