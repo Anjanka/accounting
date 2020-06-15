b4ad8fd1a980af7980090c96d3b6925222381297
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-06-15 14:17:56 +0200

    fix show function for single digit change values

--- frontend/src/Api/General/AccountingEntryTemplateUtil.elm
+++ frontend/src/Api/General/AccountingEntryTemplateUtil.elm
@@ -28,6 +28,12 @@ updateAmountWhole aet whole = { aet | amountWhole = whole }
 updateAmountChange : AccountingEntryTemplate -> Int -> AccountingEntryTemplate
 updateAmountChange aet change = { aet | amountChange = change }
 
-show: AccountingEntryTemplate -> String
+show : AccountingEntryTemplate -> String
 show aet =
-    String.concat [aet.description, ": ", String.fromInt aet.credit, " - ", String.fromInt aet.debit, ", ", String.fromInt aet.amountWhole, ",", String.fromInt aet.amountChange  ]
+    String.concat [aet.description, ": ", String.fromInt aet.credit, " - ", String.fromInt aet.debit, ", ", String.fromInt aet.amountWhole, ",", giveDoubleDigitChange aet.amountChange  ]
+
+giveDoubleDigitChange : Int -> String
+giveDoubleDigitChange change =
+    if change < 10 then
+       String.concat ["0", String.fromInt change]
+    else String.fromInt change
