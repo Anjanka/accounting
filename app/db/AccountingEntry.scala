c8debd49147ac769ab8431e4acdc768ea5a3d68d
Anja Hildebrandt <tovija@gmx.net>
2020-06-21 16:09:13 +0200

    add company as component to AccountingEntry

--- app/db/AccountingEntry.scala
+++ app/db/AccountingEntry.scala
@@ -6,7 +6,8 @@ import io.circe.generic.JsonCodec
 import base.JsonCodecs.Implicits._
 
 @JsonCodec
-case class AccountingEntry(id: Int,
+case class AccountingEntry(companyId: Int,
+                           id: Int,
                            accountingYear: Int,
                            bookingDate: Date,
                            receiptNumber: String,
