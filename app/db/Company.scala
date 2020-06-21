bf2fd43116faa6a725e97bbe662e07fde00ad181
Anja Hildebrandt <tovija@gmx.net>
2020-06-21 15:26:51 +0200

    create company and corresponding DAO

---
+++ app/db/Company.scala
@@ -0,0 +1,10 @@
+package db
+
+import io.circe.generic.JsonCodec
+
+@JsonCodec
+case class Company(id: Int,
+                   name: String,
+                   address: String,
+                   taxNumber: String,
+                   revenueOffice: String)
