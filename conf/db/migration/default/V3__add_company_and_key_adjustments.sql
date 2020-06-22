99717c8a5cff3ab9fb616efde7a471111a2b0e02
Anja Hildebrandt <tovija@gmx.net>
2020-06-22 13:42:22 +0200

    fix issues with dropping and setting of foreign key constraints

--- conf/db/migration/default/V3__add_company_and_key_adjustments.sql
+++ conf/db/migration/default/V3__add_company_and_key_adjustments.sql
@@ -8,10 +8,18 @@ create table company (
 
 insert into company values (1, 'test_company', '','','');
 
+alter table accounting_entry
+    drop constraint accounting_entry_debit_fkey,
+    drop constraint accounting_entry_credit_fkey;
+
+alter table accounting_entry_template
+    drop constraint accounting_entry_template_debit_fkey,
+    drop constraint accounting_entry_template_credit_fkey;
+
 alter table account
     add column company_id integer;
 
-update table account
+update account
 set company_id = 1;
 
 alter table account
@@ -25,19 +33,14 @@ alter table account
 
 alter table account add primary key (company_id, id);
 
-
 alter table accounting_entry
     add column company_id integer;
 
-update table accounting_entry
+update accounting_entry
 set company_id = 1;
 
 alter table accounting_entry
     alter column company_id set not null;
-
-alter table accounting_entry
-    drop constraint accounting_entry_debit_fkey,
-    drop constraint accounting_entry_credit_fkey;
 
 alter table accounting_entry
     add constraint accounting_entry_debit_fkey foreign key (debit, company_id) references account(id, company_id) deferrable initially deferred,
@@ -47,8 +50,7 @@ alter table accounting_entry
 alter table accounting_entry
     drop constraint accounting_entry_pkey;
 
-alter table accounting_entry add primary key (company_id, id, accounting_year)
-
+alter table accounting_entry add primary key (company_id, id, accounting_year);
 
 alter table accounting_entry_template
     alter column amount_whole set not null,
@@ -57,15 +59,11 @@ alter table accounting_entry_template
 alter table accounting_entry_template
     add column company_id integer;
 
-update table accounting_entry_template
+update accounting_entry_template
 set company_id = 1;
 
 alter table accounting_entry_template
     alter column company_id set not null;
-
-alter table accounting_entry_template
-    drop constraint accounting_entry_template_debit_fkey,
-    drop constraint accounting_entry_template_credit_fkey;
 
 alter table accounting_entry_template
     add constraint accounting_entry_template_debit_fkey foreign key (debit, company_id) references account(id, company_id) deferrable initially deferred,
@@ -76,5 +74,3 @@ alter table accounting_entry_template
     drop constraint accounting_entry_template_pkey;
 
 alter table accounting_entry_template add primary key (company_id, description)
-
-
