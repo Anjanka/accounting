7cc2030a3f50dd4a3dc85ce02a64bda1897ec0e5
Anja Hildebrandt <tovija@gmx.net>
2020-06-22 11:36:47 +0200

    adjust to new type structure including company_id

--- frontend/src/Api/Types/Account.elm
+++ frontend/src/Api/Types/Account.elm
@@ -5,12 +5,12 @@ import Json.Decode.Pipeline exposing (..)
 import Json.Encode as Encode
 
 
-type alias Account = { id: Int, title: String }
+type alias Account = { companyId: Int, id: Int, title: String }
 
 
 decoderAccount : Decode.Decoder Account
-decoderAccount = Decode.succeed Account |> required "id" Decode.int |> required "title" Decode.string
+decoderAccount = Decode.succeed Account |> required "companyId" Decode.int |> required "id" Decode.int |> required "title" Decode.string
 
 
 encoderAccount : Account -> Encode.Value
-encoderAccount obj = Encode.object [ ("id", Encode.int obj.id), ("title", Encode.string obj.title) ]
+encoderAccount obj = Encode.object [ ("companyId", Encode.int obj.companyId), ("id", Encode.int obj.id), ("title", Encode.string obj.title) ]
