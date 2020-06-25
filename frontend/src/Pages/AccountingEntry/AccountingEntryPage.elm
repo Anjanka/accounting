60edba87970be963378d669db6b71950b1712f92
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-06-25 15:46:10 +0200

    rearrange file structure

--- frontend/src/Pages/AccountingEntry/AccountingEntryPage.elm
+++ frontend/src/Pages/AccountingEntry/AccountingEntryPage.elm
@@ -1,23 +1,23 @@
-module Pages.AccountingEntryPage exposing (..)
+module Pages.AccountingEntry.AccountingEntryPage exposing (..)
 
-import Api.General.AccountUtil as AccountUtil
 import Api.General.AccountingEntryTemplateUtil as AccountingEntryTemplateUtil
 import Api.General.AccountingEntryUtil as AccountingEntryUtil
 import Api.General.DateUtil as DateUtil
-import Api.Types.Account exposing (Account, decoderAccount, encoderAccount)
+import Api.General.HttpUtil as HttpUtil
+import Api.Types.Account exposing (Account, decoderAccount)
 import Api.Types.AccountingEntry exposing (AccountingEntry, decoderAccountingEntry, encoderAccountingEntry)
 import Api.Types.AccountingEntryTemplate exposing (AccountingEntryTemplate, decoderAccountingEntryTemplate, encoderAccountingEntryTemplate)
-import Api.Types.Date exposing (Date, encoderDate)
 import Browser
 import Dropdown exposing (Item)
 import Html exposing (Html, button, div, input, label, table, td, text, th, tr)
-import Html.Attributes exposing (class, disabled, for, id, placeholder, style, type_, value)
+import Html.Attributes exposing (class, disabled, for, id, placeholder, value)
 import Html.Events exposing (onClick, onInput)
 import Http exposing (Error)
 import Json.Decode as Decode
-import Json.Encode as Encode
 import List.Extra
-import Pages.HttpUtil as HttpUtil
+import Pages.AccountingEntry.AccountingEntryPageModel exposing (Model)
+import Pages.AccountingEntry.ParseAndUpdateUtil as ParseAndUpdateUtil
+
 
 
 
@@ -36,29 +36,6 @@ main =
 
 -- MODEL
 
-
-type alias Model =
-    { companyId : Int
-    , accountingYear : Int
-    , contentBookingDate : String
-    , contentReceiptNumber : String
-    , contentDescription : String
-    , contentDebitID : String
-    , contentCreditID : String
-    , contentAmount : String
-    , accountingEntry : AccountingEntry
-    , allAccountingEntries : List AccountingEntry
-    , allAccounts : List Account
-    , allAccountingEntryTemplates : List AccountingEntryTemplate
-    , dateValidation : String
-    , response : String
-    , feedback : String
-    , error : String
-    , editActive : Bool
-    , selectedTemplate : Maybe String
-    , selectedCredit : Maybe String
-    , selectedDebit : Maybe String
-    }
 
 
 updateAccountingEntry : Model -> AccountingEntry -> Model
@@ -201,7 +178,7 @@ update msg model =
             ( model, getAccountingEntriesForCurrentYear model.companyId model.accountingYear )
 
         ChangeBookingDate newContent ->
-            ( parseDate model newContent, Cmd.none )
+            ( ParseAndUpdateUtil.parseDate model newContent, Cmd.none )
 
         ChangeReceiptNumber newContent ->
             ( { model | contentReceiptNumber = newContent, accountingEntry = AccountingEntryUtil.updateReceiptNumber model.accountingEntry newContent }, Cmd.none )
@@ -216,22 +193,22 @@ update msg model =
             ( { newModel | contentDescription = newContent }, Cmd.none )
 
         ChangeDebit newContent ->
-            ( parseAndUpdateDebit model newContent, Cmd.none )
+            ( ParseAndUpdateUtil.parseAndUpdateDebit model newContent, Cmd.none )
 
         ChangeCredit newContent ->
-            ( parseAndUpdateCredit model newContent, Cmd.none )
+            ( ParseAndUpdateUtil.parseAndUpdateCredit model newContent, Cmd.none )
 
         ChangeAmount newContent ->
-            ( parseAndUpdateAmount model newContent, Cmd.none )
+            (ParseAndUpdateUtil.parseAndUpdateAmount model newContent, Cmd.none )
 
         DropdownTemplateChanged selectedTemplate ->
             ( insertTemplateData model selectedTemplate, Cmd.none )
 
         DropdownCreditChanged selectedCredit ->
-            ( updateCredit model selectedCredit, Cmd.none )
+            ( ParseAndUpdateUtil.updateCredit model selectedCredit, Cmd.none )
 
         DropdownDebitChanged selectedDebit ->
-            ( updateDebit model selectedDebit, Cmd.none )
+            ( ParseAndUpdateUtil.updateDebit model selectedDebit, Cmd.none )
 
         PostAccountingEntry ->
             ( resetOnSuccessfulPost model, postAccountingEntry model.accountingEntry )
@@ -339,56 +316,6 @@ postAccountingEntry accountingEntry =
         }
 
 
-parseAndUpdateCredit : Model -> String -> Model
-parseAndUpdateCredit =
-    parseWith (\m nc -> { m | contentCreditID = nc, selectedCredit = Nothing }) (\m nc acc -> { m | contentCreditID = nc, accountingEntry = AccountingEntryUtil.updateCredit m.accountingEntry acc.id, selectedCredit = Just nc })
-
-
-parseAndUpdateDebit : Model -> String -> Model
-parseAndUpdateDebit =
-    parseWith (\m nc -> { m | contentDebitID = nc, selectedDebit = Nothing }) (\m nc acc -> { m | contentDebitID = nc, accountingEntry = AccountingEntryUtil.updateDebit m.accountingEntry acc.id, selectedDebit = Just nc })
-
-
-parseWith : (Model -> String -> Model) -> (Model -> String -> Account -> Model) -> Model -> String -> Model
-parseWith empty nonEmpty model newContent =
-    let
-        account =
-            findAccountName model.allAccounts newContent
-    in
-    if String.isEmpty account.title then
-        empty model newContent
-
-    else
-        nonEmpty model newContent account
-
-
-updateCredit : Model -> Maybe String -> Model
-updateCredit =
-    updateWith (\m nsv -> { m | selectedCredit = nsv }) (\m nsv nss id -> { m | contentCreditID = nss, accountingEntry = AccountingEntryUtil.updateCredit m.accountingEntry id, selectedCredit = nsv })
-
-
-updateDebit =
-    updateWith (\m nsv -> { m | selectedDebit = nsv }) (\m nsv nss id -> { m | contentDebitID = nss, accountingEntry = AccountingEntryUtil.updateDebit m.accountingEntry id, selectedDebit = nsv })
-
-
-updateWith : (Model -> Maybe String -> Model) -> (Model -> Maybe String -> String -> Int -> Model) -> Model -> Maybe String -> Model
-updateWith nothing just model newSelectedValue =
-    case newSelectedValue of
-        Just newSelectedString ->
-            let
-                id =
-                    String.toInt newSelectedString
-            in
-            case id of
-                Just int ->
-                    just model newSelectedValue newSelectedString int
-
-                Nothing ->
-                    nothing model newSelectedValue
-
-        Nothing ->
-            nothing model newSelectedValue
-
 
 insertTemplateData : Model -> Maybe String -> Model
 insertTemplateData model newSelectedTemplate =
@@ -426,6 +353,20 @@ insertTemplateData model newSelectedTemplate =
             { model | selectedTemplate = newSelectedTemplate }
 
 
+findEntry : Maybe String -> List AccountingEntryTemplate -> AccountingEntryTemplate
+findEntry selectedValue allAccountingEntryTemplates =
+    case selectedValue of
+        Just string ->
+            case List.Extra.find (\aet -> aet.description == string) allAccountingEntryTemplates of
+                Just value ->
+                    value
+
+                Nothing ->
+                    AccountingEntryTemplateUtil.empty
+
+        Nothing ->
+            AccountingEntryTemplateUtil.empty
+
 insertForEdit : Model -> AccountingEntry -> Model
 insertForEdit model accountingEntry =
     { model
@@ -443,191 +384,6 @@ insertForEdit model accountingEntry =
     }
 
 
-findAccountName : List Account -> String -> Account
-findAccountName accounts id =
-    case String.toInt id of
-        Just int ->
-            case List.Extra.find (\acc -> acc.id == int) accounts of
-                Just value ->
-                    value
-
-                Nothing ->
-                    AccountUtil.empty
-
-        Nothing ->
-            AccountUtil.empty
-
-
-findEntry : Maybe String -> List AccountingEntryTemplate -> AccountingEntryTemplate
-findEntry selectedValue allAccountingEntryTemplates =
-    case selectedValue of
-        Just string ->
-            case List.Extra.find (\aet -> aet.description == string) allAccountingEntryTemplates of
-                Just value ->
-                    value
-
-                Nothing ->
-                    AccountingEntryTemplateUtil.empty
-
-        Nothing ->
-            AccountingEntryTemplateUtil.empty
-
-
-parseAndUpdateAmount : Model -> String -> Model
-parseAndUpdateAmount model newContent =
-    if String.isEmpty newContent then
-        { model | contentAmount = "", accountingEntry = AccountingEntryUtil.updateCompleteAmount model.accountingEntry 0 0 }
-
-    else
-        let
-            wholeAndChange =
-                String.split "," newContent
-        in
-        case List.head wholeAndChange of
-            Just wholeString ->
-                case String.toInt wholeString of
-                    Just whole ->
-                        case List.tail wholeAndChange of
-                            Just tailList ->
-                                case List.head tailList of
-                                    Just changeString ->
-                                        case String.toInt (String.left 2 changeString) of
-                                            Just change ->
-                                                if change < 10 && String.length changeString == 1 then
-                                                    { model | contentAmount = String.concat [ String.fromInt whole, ",", String.fromInt change ], accountingEntry = AccountingEntryUtil.updateAmountWhole (AccountingEntryUtil.updateAmountChange model.accountingEntry (change * 10)) whole }
-
-                                                else if change < 10 && String.length changeString >= 2 then
-                                                    { model | contentAmount = String.concat [ String.fromInt whole, ",0", String.fromInt change ], accountingEntry = AccountingEntryUtil.updateAmountWhole (AccountingEntryUtil.updateAmountChange model.accountingEntry change) whole }
-
-                                                else
-                                                    { model | contentAmount = String.concat [ String.fromInt whole, ",", String.fromInt change ], accountingEntry = AccountingEntryUtil.updateAmountWhole (AccountingEntryUtil.updateAmountChange model.accountingEntry change) whole }
-
-                                            Nothing ->
-                                                { model | contentAmount = String.concat [ String.fromInt whole, "," ], accountingEntry = AccountingEntryUtil.updateCompleteAmount model.accountingEntry whole 0 }
-
-                                    Nothing ->
-                                        { model | contentAmount = newContent, accountingEntry = AccountingEntryUtil.updateCompleteAmount model.accountingEntry whole 0 }
-
-                            Nothing ->
-                                { model | contentAmount = newContent, accountingEntry = AccountingEntryUtil.updateCompleteAmount model.accountingEntry whole 0 }
-
-                    Nothing ->
-                        model
-
-            Nothing ->
-                model
-
-
-parseDate : Model -> String -> Model
-parseDate model newContent =
-    if String.isEmpty newContent then
-        { model | contentBookingDate = "" }
-
-    else
-        let
-            dayAndMonth =
-                String.split "." newContent
-        in
-        case List.head dayAndMonth of
-            Just dayString ->
-                case toValidDay dayString of
-                    Just day ->
-                        if String.length dayString == 2 && day /= 0 then
-                            case List.tail dayAndMonth of
-                                Just tailList ->
-                                    case List.head tailList of
-                                        Just monthString ->
-                                            case toValidMonth monthString day model.accountingYear of
-                                                Just month ->
-                                                    { model
-                                                        | contentBookingDate = DateUtil.showDay day ++ "." ++ DateUtil.showMonth month
-                                                        , accountingEntry = AccountingEntryUtil.updateBookingDate model.accountingEntry { day = day, month = month, year = model.accountingYear }
-                                                    }
-
-                                                Nothing ->
-                                                    { model
-                                                        | contentBookingDate = DateUtil.showDay day ++ "."
-                                                        , accountingEntry = AccountingEntryUtil.updateBookingDate model.accountingEntry { day = day, month = 0, year = model.accountingYear }
-                                                    }
-
-                                        Nothing ->
-                                            { model
-                                                | contentBookingDate = DateUtil.showDay day ++ "."
-                                                , accountingEntry = AccountingEntryUtil.updateBookingDate model.accountingEntry { day = day, month = 0, year = model.accountingYear }
-                                            }
-
-                                Nothing ->
-                                    { model
-                                        | contentBookingDate = DateUtil.showDay day
-                                        , accountingEntry = AccountingEntryUtil.updateBookingDate model.accountingEntry { day = day, month = 0, year = model.accountingYear }
-                                    }
-
-                        else
-                            { model
-                                | contentBookingDate = DateUtil.showDay day
-                                , accountingEntry = AccountingEntryUtil.updateBookingDate model.accountingEntry { day = day, month = 0, year = model.accountingYear }
-                            }
-
-                    Nothing ->
-                        model
-
-            Nothing ->
-                model
-
-
-toValidDay : String -> Maybe Int
-toValidDay dayCandidateString =
-    case String.toInt dayCandidateString of
-        Just dayCandidateInt ->
-            if dayCandidateInt == 0 && String.length dayCandidateString <= 2 then
-                Just 0
-
-            else if 1 <= dayCandidateInt && dayCandidateInt <= 31 then
-                Just dayCandidateInt
-
-            else
-                Nothing
-
-        Nothing ->
-            Nothing
-
-
-toValidMonth : String -> Int -> Int -> Maybe Int
-toValidMonth monthCandidateString day year =
-    case String.toInt monthCandidateString of
-        Just monthCandidateInt ->
-            if monthCandidateInt == 0 && String.length monthCandidateString <= 2 then
-                Just 0
-
-            else if day == 29 && isLeap year && monthCandidateInt == 2 then
-                Just monthCandidateInt
-
-            else if day <= 28 && monthCandidateInt == 2 then
-                Just monthCandidateInt
-
-            else if day >= 29 && monthCandidateInt == 2 then
-                Nothing
-
-            else if day == 31 && List.member monthCandidateInt [ 1, 3, 5, 7, 8, 10, 12 ] then
-                Just monthCandidateInt
-
-            else if day <= 30 && monthCandidateInt <= 12 then
-                Just monthCandidateInt
-
-            else
-                Nothing
-
-        Nothing ->
-            Nothing
-
-
-isLeap : Int -> Bool
-isLeap year =
-    if modBy 4 year == 0 then
-        True
-
-    else
-        False
 
 
 accountForDropdown : Account -> Item
