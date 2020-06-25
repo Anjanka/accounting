065828c932757c76bac728f49aab71bee8858139
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-25 22:36:36 +0200

    Move generator to separate class, make table names start with a lower case letter

--- app/db/codegen/Codegen.scala
+++ app/db/codegen/Codegen.scala
@@ -100,21 +100,3 @@ object Codegen {
 
 }
 
-class CustomCodeGenerator(model: Model) extends SourceCodeGenerator(model) {
-  override def entityName: String => String = _.toCamelCase
-
-  override def tableName: String => String = name => s"${entityName(name)}Table"
-
-  override def Table = new Table(_) {
-    table =>
-    override def factory: String = {
-      val apply = s"${TableClass.elementType}.apply"
-      if (columns.size == 1) apply else s"($apply _).tupled"
-    }
-
-    override def EntityType: EntityTypeDef = new EntityTypeDef {
-      override def doc: String = ""
-    }
-
-  }
-}
