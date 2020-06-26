15c4176bebbb611ced4cc90be0f9c29b355f43ae
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-26 18:13:46 +0200

    Use code generator for tables and table entries

--- app/db/AccountingEntry.scala
+++ app/db/AccountingEntry.scala
@@ -1,18 +1,19 @@
 package db
 
 import java.sql.Date
-
 import io.circe.generic.JsonCodec
 import base.JsonCodecs.Implicits._
 
 @JsonCodec
-case class AccountingEntry(companyId: Int,
-                           id: Int,
-                           accountingYear: Int,
-                           bookingDate: Date,
-                           receiptNumber: String,
-                           description: String,
-                           credit: Int,
-                           debit: Int,
-                           amountWhole: Int,
-                           amountChange: Int)
+case class AccountingEntry(
+    id: Int,
+    accountingYear: Int,
+    bookingDate: java.sql.Date,
+    receiptNumber: String,
+    description: String,
+    credit: Int,
+    debit: Int,
+    amountWhole: Int,
+    amountChange: Int,
+    companyId: Int
+)
--- app/db/Company.scala
+++ app/db/Company.scala
@@ -1,10 +1,5 @@
 package db
 
 import io.circe.generic.JsonCodec
-
 @JsonCodec
-case class Company(id: Int,
-                   name: String,
-                   address: String,
-                   taxNumber: String,
-                   revenueOffice: String)
+case class Company(id: Int, name: String, address: String, taxnumber: String, revenueoffice: String)
