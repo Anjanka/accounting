2f315e321756a767ce3204ff2e1f16692f123791
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-26 23:59:02 +0200

    Use DAO abstraction for the implementation of the AccountingEntryDAO

--- app/db/AccountingEntryDAO.scala
+++ app/db/AccountingEntryDAO.scala
@@ -1,56 +1,48 @@
 package db
 
-import java.time.Year
-
 import base.Id
+import base.Id.AccountingEntryKey
 import javax.inject.Inject
-import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
 import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.language.higherKinds
 
-class AccountingEntryDAO @Inject()(override protected val dbConfigProvider: DatabaseConfigProvider)
-                                  (implicit executionContext: ExecutionContext)
-  extends HasDatabaseConfigProvider[PostgresProfile] {
+class AccountingEntryDAO @Inject() (override protected val dbConfigProvider: DatabaseConfigProvider)(implicit
+    executionContext: ExecutionContext
+) extends HasDatabaseConfigProvider[PostgresProfile] {
+
+  private val dao = DAO(AccountingEntryDAO.daoCompanion, dbConfigProvider)
+
   def findAccountingEntry(accountingEntryKey: Id.AccountingEntryKey): Future[Option[AccountingEntry]] =
-    db.run(AccountingEntryDAO.findAccountingEntryAction(accountingEntryKey))
+    dao.find(accountingEntryKey)
 
   def deleteAccountingEntry(accountingEntryKey: Id.AccountingEntryKey): Future[Unit] =
-    db.run(AccountingEntryDAO.deleteAccountingEntryAction(accountingEntryKey))
+    dao.delete(accountingEntryKey)
 
   def repsertAccountingEntry(accountingEntry: AccountingEntry): Future[AccountingEntry] =
-    db.run(AccountingEntryDAO.repsertAccountingEntryAction(accountingEntry))
+    dao.repsert(
+      accountingEntry,
+      dbEntry => accountingEntry.debit == dbEntry.debit && accountingEntry.credit == dbEntry.credit
+    )
 
   def findAccountingEntriesByCompanyAndYear(companyID: Int, accountingYear: Int): Future[Seq[AccountingEntry]] =
-    db.run(AccountingEntryDAO.findAccountingEntriesByCompanyAndYearAction(companyID, accountingYear))
+    dao.findPartial((companyID, accountingYear))({
+      case (entry, (cId, year)) => entry.companyId === cId && entry.accountingYear === year
+    })
+
 }
 
 object AccountingEntryDAO {
 
-  def findAccountingEntryAction(accountingEntryKey: Id.AccountingEntryKey): DBIO[Option[AccountingEntry]] =
-    fetch(accountingEntryKey).result.headOption
+  val daoCompanion: DAOCompanion[Tables.AccountingEntryTable, AccountingEntryKey] = DAOCompanion(
+    _table = Tables.accountingEntryTable,
+    _compare = (entry, key) =>
+      entry.companyId === key.companyID &&
+        entry.id === key.id &&
+        entry.accountingYear === key.accountingYear
+  )
 
-  def findAccountingEntriesByCompanyAndYearAction(companyID: Int, accountingYear: Int): DBIO[Seq[AccountingEntry]] =
-    Tables.accountingEntryTable.filter(entry => entry.companyId === companyID && entry.accountingYear === accountingYear).result
-
-  def deleteAccountingEntryAction(accountingEntryKey: Id.AccountingEntryKey)
-                                 (implicit ec: ExecutionContext): DBIO[Unit] =
-    fetch(accountingEntryKey).delete.map(_ => ())
-
-  def repsertAccountingEntryAction(accountingEntry: AccountingEntry)(implicit ec: ExecutionContext): DBIO[AccountingEntry] = {
-    Tables.accountingEntryTable.returning(Tables.accountingEntryTable).insertOrUpdate(accountingEntry).flatMap {
-      case Some(dbEntry) if accountingEntry.credit == dbEntry.credit && accountingEntry.debit == dbEntry.debit =>
-        DBIO.successful(accountingEntry)
-      case Some(dbEntry) => DBIO.failed(new Throwable(s"Inserted entry $dbEntry doesn't match given entry $accountingEntry."))
-      case None => DBIO.successful(accountingEntry)
-    }
-  }
-
-  private def fetch(accountingEntryKey: Id.AccountingEntryKey): Query[Tables.AccountingEntryTable, AccountingEntry, Seq] =
-    Tables.accountingEntryTable.filter(entry =>
-      entry.companyId === accountingEntryKey.companyID &&
-        entry.id === accountingEntryKey.id &&
-        entry.accountingYear === accountingEntryKey.accountingYear)
 }
