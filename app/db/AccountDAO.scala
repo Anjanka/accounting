e1256715b4b4302779d656015a9f314f19ee0a57
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-26 23:41:26 +0200

    Use DAO abstraction for the implementation of the Account DAO as an example

--- app/db/AccountDAO.scala
+++ app/db/AccountDAO.scala
@@ -1,6 +1,7 @@
 package db
 
 import base.Id
+import base.Id.AccountKey
 import javax.inject.Inject
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.PostgresProfile
@@ -11,26 +12,25 @@ import scala.concurrent.{ExecutionContext, Future}
 class AccountDAO @Inject()(override protected val dbConfigProvider: DatabaseConfigProvider)
                           (implicit executionContext: ExecutionContext)
   extends HasDatabaseConfigProvider[PostgresProfile] {
-  def findAccount(accountKey: Id.AccountKey): Future[Option[Account]] = db.run(AccountDAO.findAccountAction(accountKey))
+  private val dao: DAO[Tables.AccountTable, AccountKey] = DAO(AccountDAO.daoCompanion, dbConfigProvider)
 
-  def deleteAccount(accountKey: Id.AccountKey): Future[Unit] = db.run(AccountDAO.deleteAccountAction(accountKey))
+  def findAccount(accountKey: Id.AccountKey): Future[Option[Account]] = dao.find(accountKey)
 
-  def repsertAccount(account: Account): Future[Account] = db.run(AccountDAO.repsertAccountAction(account))
+  def deleteAccount(accountKey: Id.AccountKey): Future[Unit] = dao.delete(accountKey)
 
-  def getAllAccountsByCompany(companyID: Int): Future[Seq[Account]] = db.run(AccountDAO.getAllAccountsByCompanyAction(companyID))
+  def repsertAccount(account: Account): Future[Account] = dao.repsert(account)
+
+  def getAllAccountsByCompany(companyID: Int): Future[Seq[Account]] =
+    dao.findPartial[Int]((acc, cId) => acc.companyId === cId)(companyID)
 }
 
 object AccountDAO {
 
-  def findAccountAction(accountKey: Id.AccountKey): DBIO[Option[Account]] = Tables.accountTable.filter(acc => acc.id === accountKey.id && acc.companyId === accountKey.companyID).result.headOption
+  import PostgresProfile.api._
 
-  def deleteAccountAction(accountKey: Id.AccountKey)(implicit ec: ExecutionContext): DBIO[Unit] = Tables.accountTable.filter(acc => acc.id === accountKey.id && acc.companyId === accountKey.companyID).delete.map(_ => ())
-
-  def repsertAccountAction(account: Account)(implicit ec: ExecutionContext): DBIO[Account] = Tables.accountTable.returning(Tables.accountTable).insertOrUpdate(account).map {
-    case Some(value) => value
-    case None => account
-  }
-
-  def getAllAccountsByCompanyAction(companyID: Int): DBIO[Seq[Account]] = Tables.accountTable.filter(acc => acc.companyId === companyID).result
+  val daoCompanion: DAOCompanion[Tables.AccountTable, AccountKey] = DAOCompanion(
+    _table = Tables.accountTable,
+    _compare = (acc, accountKey) => acc.id === accountKey.id && acc.companyId === accountKey.companyID
+  )
 
 }
