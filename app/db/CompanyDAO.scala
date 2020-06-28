2545f610208f81a39efe5a8fcb5616835c815dcb
Anja Hildebrandt <tovija@gmx.net>
2020-06-28 15:24:26 +0200

    Refactor company DAO to only use generic operations

--- app/db/CompanyDAO.scala
+++ app/db/CompanyDAO.scala
@@ -1,36 +1,31 @@
 package db
 
-import base.Id
+import base.Id.CompanyKey
 import javax.inject.Inject
-import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
 import slick.jdbc.PostgresProfile
-import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
-class CompanyDAO @Inject()(override protected val dbConfigProvider: DatabaseConfigProvider)
-                          (implicit executionContext: ExecutionContext)
-  extends HasDatabaseConfigProvider[PostgresProfile] {
-  def findCompany(companyKey : Id.CompanyKey): Future[Option[Company]] = db.run(CompanyDAO.findCompanyAction(companyKey))
-
-  def deleteCompany(companyKey : Id.CompanyKey): Future[Unit] = db.run(CompanyDAO.deleteCompanyAction(companyKey))
-
-  def repsertCompany(company: Company): Future[Company] = db.run(CompanyDAO.repsertCompanyAction(company))
-
-  def getAllCompanies: Future[Seq[Company]] = db.run(CompanyDAO.getAllCompaniesAction)
+class CompanyDAO @Inject() (override protected val dbConfigProvider: DatabaseConfigProvider)(implicit
+    executionContext: ExecutionContext
+) extends HasDatabaseConfigProvider[PostgresProfile] {
+  val dao: DAO[Company, Tables.CompanyTable, CompanyKey] = DAO(CompanyDAO.daoCompanion, dbConfigProvider)
 }
 
 object CompanyDAO {
 
-  def findCompanyAction(companyKey : Id.CompanyKey): DBIO[Option[Company]] = Tables.companyTable.filter(com => com.id === companyKey.id).result.headOption
+  import PostgresProfile.api._
 
-  def deleteCompanyAction(companyKey : Id.CompanyKey)(implicit ec: ExecutionContext): DBIO[Unit] = Tables.companyTable.filter(com => com.id === companyKey.id).delete.map(_ => ())
+  val daoCompanion: DAOCompanion[Company, Tables.CompanyTable, CompanyKey] = DAOCompanion(
+    _table = Tables.companyTable,
+    _compare = (comp, companyKey) => comp.id === companyKey.id
+  )
 
-  def repsertCompanyAction(company: Company)(implicit ec: ExecutionContext): DBIO[Company] = Tables.companyTable.returning(Tables.companyTable).insertOrUpdate(company).map {
-    case Some(value) => value
-    case None => company
-  }
-
-  def getAllCompaniesAction : DBIO[Seq[Company]] = Tables.companyTable.result
+  def nextId(implicit ec: ExecutionContext): DBIO[Int] =
+    daoCompanion.allAction.map { companies =>
+      if (companies.isEmpty) 1
+      else companies.maxBy(_.id).id + 1
+    }
 
 }
