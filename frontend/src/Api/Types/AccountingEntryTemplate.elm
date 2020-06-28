9f103dfddcebb56a3541f86bf1a58dc94d043ff7
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-06-28 12:46:29 +0200

    Add bridged types, regenerate Elm types

--- frontend/src/Api/Types/AccountingEntryTemplate.elm
+++ frontend/src/Api/Types/AccountingEntryTemplate.elm
@@ -5,12 +5,12 @@ import Json.Decode.Pipeline exposing (..)
 import Json.Encode as Encode
 
 
-type alias AccountingEntryTemplate = { companyId: Int, description: String, credit: Int, debit: Int, amountWhole: Int, amountChange: Int }
+type alias AccountingEntryTemplate = { description: String, credit: Int, debit: Int, amountWhole: Int, amountChange: Int, companyId: Int }
 
 
 decoderAccountingEntryTemplate : Decode.Decoder AccountingEntryTemplate
-decoderAccountingEntryTemplate = Decode.succeed AccountingEntryTemplate |> required "companyId" Decode.int |> required "description" Decode.string |> required "credit" Decode.int |> required "debit" Decode.int |> required "amountWhole" Decode.int |> required "amountChange" Decode.int
+decoderAccountingEntryTemplate = Decode.succeed AccountingEntryTemplate |> required "description" Decode.string |> required "credit" Decode.int |> required "debit" Decode.int |> required "amountWhole" Decode.int |> required "amountChange" Decode.int |> required "companyId" Decode.int
 
 
 encoderAccountingEntryTemplate : AccountingEntryTemplate -> Encode.Value
-encoderAccountingEntryTemplate obj = Encode.object [ ("companyId", Encode.int obj.companyId), ("description", Encode.string obj.description), ("credit", Encode.int obj.credit), ("debit", Encode.int obj.debit), ("amountWhole", Encode.int obj.amountWhole), ("amountChange", Encode.int obj.amountChange) ]
+encoderAccountingEntryTemplate obj = Encode.object [ ("description", Encode.string obj.description), ("credit", Encode.int obj.credit), ("debit", Encode.int obj.debit), ("amountWhole", Encode.int obj.amountWhole), ("amountChange", Encode.int obj.amountChange), ("companyId", Encode.int obj.companyId) ]
