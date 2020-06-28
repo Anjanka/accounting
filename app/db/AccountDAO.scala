d90be1ae79184a98892086c4567ff8c0aaba1a58
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-28 16:26:00 +0200

    Use DAO abstraction for account DAO

--- app/db/AccountDAO.scala
+++ app/db/AccountDAO.scala
@@ -1,33 +1,25 @@
 package db
 
-import base.Id
 import base.Id.AccountKey
+import db.DAOCompanion.FindPredicate
 import javax.inject.Inject
-import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.PostgresProfile
-import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.ExecutionContext
 
 class AccountDAO @Inject() (override protected val dbConfigProvider: DatabaseConfigProvider)(implicit
     executionContext: ExecutionContext
 ) extends HasDatabaseConfigProvider[PostgresProfile] {
-  private val dao: DAO[Account, Tables.AccountTable, AccountKey] = DAO(AccountDAO.daoCompanion, dbConfigProvider)
-
-  def findAccount(accountKey: Id.AccountKey): Future[Option[Account]] = dao.find(accountKey)
-
-  def deleteAccount(accountKey: Id.AccountKey): Future[Unit] = dao.delete(accountKey)
-
-  def repsertAccount(account: Account): Future[Account] = dao.repsert(account)
-
-  def getAllAccountsByCompany(companyID: Int): Future[Seq[Account]] =
-    dao.findPartial(companyID)((acc, cId) => acc.companyId === cId)
-
+  val dao: DAO[Account, Tables.AccountTable, AccountKey] = DAO(AccountDAO.daoCompanion, dbConfigProvider)
 }
 
 object AccountDAO {
 
   import PostgresProfile.api._
+
+  val compareByCompany: FindPredicate[Tables.AccountTable, Int] =
+    (acc, cId) => acc.companyId === cId
 
   val daoCompanion: DAOCompanion[Account, Tables.AccountTable, AccountKey] = DAOCompanion(
     _table = Tables.accountTable,
