329be19fb2547cb3f87c171e0787ab424d40da6b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-30 18:09:18 +0200

    Split repsert operation for Account into replace and insert

--- app/db/Account.scala
+++ app/db/Account.scala
@@ -1,5 +1,14 @@
 package db
 
+import base.Id.AccountKey
 import io.circe.generic.JsonCodec
+
 @JsonCodec
 case class Account(id: Int, title: String, companyId: Int)
+
+object Account {
+
+  def keyOf(account: Account): AccountKey =
+    AccountKey(companyID = account.companyId, id = account.id)
+
+}
