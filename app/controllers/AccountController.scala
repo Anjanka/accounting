5504b0abd84b424dc5e4895d8fbea91ed7d693e3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-07-01 00:10:59 +0200

    Add 'findPartial' function and use it in all relevant controllers; adjust routes

--- app/controllers/AccountController.scala
+++ app/controllers/AccountController.scala
@@ -3,9 +3,7 @@ package controllers
 import base.Id.AccountKey
 import db.{Account, AccountDAO, Tables}
 import io.circe.Json
-import io.circe.syntax._
 import javax.inject.{Inject, Singleton}
-import play.api.libs.circe.Circe
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import slick.dbio.DBIO
 
@@ -14,8 +12,7 @@ import scala.concurrent.ExecutionContext
 @Singleton
 class AccountController @Inject() (val controllerComponents: ControllerComponents, val accountDAO: AccountDAO)(implicit
     ec: ExecutionContext
-) extends BaseController
-    with Circe {
+) extends BaseController {
 
   val controller: Controller[Account, Tables.AccountTable, AccountKey, Account, Unit] =
     Controller[Account, Tables.AccountTable, AccountKey, Account, Unit](
@@ -39,11 +36,7 @@ class AccountController @Inject() (val controllerComponents: ControllerComponent
   def delete: Action[Json] =
     controller.delete
 
-  def getAll(companyID: Int): Action[AnyContent] =
-    Action.async {
-      accountDAO.dao.findPartial(companyID)(AccountDAO.compareByCompany).map { x =>
-        Ok(x.asJson)
-      }
-    }
+  def findAll(companyID: Int): Action[AnyContent] =
+    controller.findPartial(companyID)(AccountDAO.compareByCompany)
 
 }
--- app/controllers/AccountingEntryController.scala
+++ app/controllers/AccountingEntryController.scala
@@ -3,12 +3,10 @@ package controllers
 import base.Id.AccountingEntryKey
 import db.AccountingEntryDAO.CompanyYearKey
 import db.creation.AccountingEntryCreationParams
-import db.{AccountingEntry, AccountingEntryDAO, Tables}
+import db.{ AccountingEntry, AccountingEntryDAO, Tables }
 import io.circe.Json
-import io.circe.syntax._
-import javax.inject.{Inject, Singleton}
-import play.api.libs.circe.Circe
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import javax.inject.{ Inject, Singleton }
+import play.api.mvc.{ Action, AnyContent, BaseController, ControllerComponents }
 
 import scala.concurrent.ExecutionContext
 
@@ -17,8 +15,7 @@ class AccountingEntryController @Inject() (
     val controllerComponents: ControllerComponents,
     val accountingEntryDAO: AccountingEntryDAO
 )(implicit ec: ExecutionContext)
-    extends BaseController
-    with Circe {
+    extends BaseController {
 
   val controller: Controller[
     AccountingEntry,
@@ -40,13 +37,7 @@ class AccountingEntryController @Inject() (
     controller.find(AccountingEntryKey(companyID = companyID, id = id, accountingYear = accountingYear))
 
   def findByYear(companyID: Int, accountingYear: Int): Action[AnyContent] =
-    Action.async {
-      accountingEntryDAO.dao
-        .findPartial(CompanyYearKey(companyID, accountingYear))(AccountingEntryDAO.compareCompanyYearKey)
-        .map { x =>
-          Ok(x.asJson)
-        }
-    }
+    controller.findPartial(CompanyYearKey(companyID, accountingYear))(AccountingEntryDAO.compareCompanyYearKey)
 
   def replace: Action[Json] =
     controller.replace
