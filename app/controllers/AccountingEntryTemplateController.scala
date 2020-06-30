834b11d2e74b3cf8039c6788fb4b57c0e1d396a6
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-06-30 23:44:31 +0200

    Use generic pattern for implementation

--- app/controllers/AccountingEntryTemplateController.scala
+++ app/controllers/AccountingEntryTemplateController.scala
@@ -1,15 +1,15 @@
 package controllers
 
-import base.Id
+import base.Id.AccountingEntryTemplateKey
 import db.creation.AccountingEntryTemplateCreationParams
-import db.{AccountingEntryTemplate, AccountingEntryTemplateDAO}
+import db.{AccountingEntryTemplate, AccountingEntryTemplateDAO, Tables}
 import io.circe.Json
 import io.circe.syntax._
 import javax.inject.{Inject, Singleton}
 import play.api.libs.circe.Circe
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class AccountingEntryTemplateController @Inject() (
@@ -19,60 +19,27 @@ class AccountingEntryTemplateController @Inject() (
     extends BaseController
     with Circe {
 
-  def find(id: Int): Action[AnyContent] =
-    Action.async {
+  val controller: Controller[AccountingEntryTemplate, Tables.AccountingEntryTemplateTable, AccountingEntryTemplateKey, AccountingEntryTemplateCreationParams, Int] =
+    Controller[AccountingEntryTemplate, Tables.AccountingEntryTemplateTable, AccountingEntryTemplateKey, AccountingEntryTemplateCreationParams, Int](
+      _ => AccountingEntryTemplateDAO.nextId,
+      AccountingEntryTemplateCreationParams.create,
+      AccountingEntryTemplate.keyOf,
       accountingEntryTemplateDAO.dao
-        .find(Id.AccountingEntryTemplateKey(id = id))
-        .map { x =>
-          Ok(x.asJson)
-        }
-    }
-
-  def insert: Action[Json] =
-    Action.async(circe.json) { request =>
-      val companyCreationParamsCandidate = request.body.as[AccountingEntryTemplateCreationParams]
-      companyCreationParamsCandidate match {
-        case Right(companyCreationParams) =>
-          accountingEntryTemplateDAO.dao
-            .insert(AccountingEntryTemplateCreationParams.create)(_ => AccountingEntryTemplateDAO.nextId)(companyCreationParams)
-            .map(acc => Ok(acc.asJson))
-        case Left(decodingFailure) =>
-          Future(BadRequest(s"Could not parse ${request.body} as valid accounting entry template creation params: $decodingFailure"))
-      }
-    }
-
-  def replace: Action[Json] =
-    processAccountingEntryTemplateWith(
-      accountingEntryTemplateDAO.dao.replace(_)(AccountingEntryTemplate.keyOf)
+    )(
+      controllerComponents
     )
 
-  private def processAccountingEntryTemplateWith(
-      f: AccountingEntryTemplate => Future[AccountingEntryTemplate]
-  ): Action[Json] =
-    Action.async(circe.json) { request =>
-      val accountingEntryTemplateCandidate = request.body.as[AccountingEntryTemplate]
-      accountingEntryTemplateCandidate match {
-        case Right(value) =>
-          f(value).map(acc => Ok(acc.asJson))
-        case Left(decodingFailure) =>
-          Future(BadRequest(s"Could not parse ${request.body} as valid accounting entry template: $decodingFailure"))
-      }
-    }
+  def find(id: Int): Action[AnyContent] =
+    controller.find(AccountingEntryTemplateKey(id = id))
+
+  def insert: Action[Json] =
+    controller.insert
+
+  def replace: Action[Json] =
+    controller.replace
 
   def delete: Action[Json] =
-    Action.async(circe.json) { request =>
-      val accountIdCandidate = request.body.as[Id.AccountingEntryTemplateKey]
-      accountIdCandidate match {
-        case Right(value) =>
-          accountingEntryTemplateDAO.dao
-            .delete(value)
-            .map(_ => Ok(s"Accounting entry template '${value.id}' was deleted successfully."))
-        case Left(decodingFailure) =>
-          Future(
-            BadRequest(s"Could not parse ${request.body} as valid accounting entry template Id: $decodingFailure.")
-          )
-      }
-    }
+    controller.delete
 
   def getAll(companyID: Int): Action[AnyContent] =
     Action.async {
