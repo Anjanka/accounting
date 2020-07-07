d5c4a807da2f08d26502ab967f36e7c1a4d467ae
Anja Hildebrandt <tovija@gmx.net>
2020-07-07 17:14:24 +0200

    separate update and parse functions for month and day

--- frontend/src/Pages/AccountingEntry/AccountingEntryPage.elm
+++ frontend/src/Pages/AccountingEntry/AccountingEntryPage.elm
@@ -18,7 +18,7 @@ import Json.Decode as Decode
 import Pages.AccountingEntry.AccountingEntryPageModel exposing (Model)
 import Pages.AccountingEntry.HelperUtil exposing (getBalance, handleAccountSelection, handleSelection, insertForEdit, insertTemplateData, reset)
 import Pages.AccountingEntry.InputContent exposing (emptyInputContent)
-import Pages.AccountingEntry.ParseAndUpdateUtil exposing (parseAndUpdateAmount, parseAndUpdateCredit, parseAndUpdateDebit, parseDay, parseMonth, updateCredit, updateDebit, updateDescription, updateReceiptNumber)
+import Pages.AccountingEntry.ParseAndUpdateUtil exposing (parseAndUpdateAmount, parseAndUpdateCredit, parseAndUpdateDebit, parseDay, parseMonth, updateCredit, updateDay, updateDebit, updateDescription, updateMonth, updateReceiptNumber)
 import Pages.SharedViewComponents exposing (accountForDropdown, accountListForDropdown)
 
 
@@ -160,10 +160,10 @@ update msg model =
                     ( { model | error = HttpUtil.errorToString error }, Cmd.none )
 
         ChangeDay newContent ->
-            ( parseDay model newContent, Cmd.none )
+            ( updateDay model (parseDay model newContent), Cmd.none )
 
         ChangeMonth newContent ->
-            ( parseMonth model newContent, Cmd.none )
+            ( updateMonth model (parseMonth model newContent), Cmd.none )
 
         ChangeReceiptNumber newContent ->
            ( updateReceiptNumber model newContent, Cmd.none )
