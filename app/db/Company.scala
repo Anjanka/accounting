cab6c07ffdfb433932f77b769989be03c1fdaf58
Anja Hildebrandt <tovija@gmx.net>
2020-07-16 21:59:07 +0200

    add separate address fields to company

--- app/db/Company.scala
+++ app/db/Company.scala
@@ -2,9 +2,17 @@ package db
 
 import base.Id.CompanyKey
 import io.circe.generic.JsonCodec
-@JsonCodec
-case class Company(id: Int, name: String, address: String, taxNumber: String, revenueOffice: String)
 
-object Company {
-  def keyOf(company: Company): CompanyKey = CompanyKey(id = company.id)
-}
+@JsonCodec
+case class Company(
+    id: Int,
+    name: String,
+    address: String,
+    taxNumber: String,
+    revenueOffice: String,
+    postalCode: String,
+    city: String,
+    country: String
+)
+
+object Company { def keyOf(company: Company): CompanyKey = CompanyKey(id = company.id) }
--- frontend/src/Api/Types/Company.elm
+++ frontend/src/Api/Types/Company.elm
@@ -5,12 +5,12 @@ import Json.Decode.Pipeline exposing (..)
 import Json.Encode as Encode
 
 
-type alias Company = { id: Int, name: String, address: String, taxNumber: String, revenueOffice: String }
+type alias Company = { id: Int, name: String, address: String, taxNumber: String, revenueOffice: String, postalCode: String, city: String, country: String }
 
 
 decoderCompany : Decode.Decoder Company
-decoderCompany = Decode.succeed Company |> required "id" Decode.int |> required "name" Decode.string |> required "address" Decode.string |> required "taxNumber" Decode.string |> required "revenueOffice" Decode.string
+decoderCompany = Decode.succeed Company |> required "id" Decode.int |> required "name" Decode.string |> required "address" Decode.string |> required "taxNumber" Decode.string |> required "revenueOffice" Decode.string |> required "postalCode" Decode.string |> required "city" Decode.string |> required "country" Decode.string
 
 
 encoderCompany : Company -> Encode.Value
-encoderCompany obj = Encode.object [ ("id", Encode.int obj.id), ("name", Encode.string obj.name), ("address", Encode.string obj.address), ("taxNumber", Encode.string obj.taxNumber), ("revenueOffice", Encode.string obj.revenueOffice) ]
+encoderCompany obj = Encode.object [ ("id", Encode.int obj.id), ("name", Encode.string obj.name), ("address", Encode.string obj.address), ("taxNumber", Encode.string obj.taxNumber), ("revenueOffice", Encode.string obj.revenueOffice), ("postalCode", Encode.string obj.postalCode), ("city", Encode.string obj.city), ("country", Encode.string obj.country) ]
