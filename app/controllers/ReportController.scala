9b29a2aef0b2133cfe499c6bdf0055c54565c271
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-07-23 22:15:06 +0200

    Set up more information for the XML containing report information

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -3,7 +3,8 @@ package controllers
 import java.io.ByteArrayInputStream
 
 import akka.stream.scaladsl.StreamConverters
-import db.AccountingEntryDAO
+import base.Id.CompanyKey
+import db.{AccountingEntryDAO, CompanyDAO}
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import report.{ReportCreator, TestMe}
@@ -11,17 +12,23 @@ import report.{ReportCreator, TestMe}
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
 
-class ReportController @Inject() (accountingEntryDAO: AccountingEntryDAO,
-                                  val controllerComponents: ControllerComponents)(implicit
+class ReportController @Inject() (
+    accountingEntryDAO: AccountingEntryDAO,
+    companyDAO: CompanyDAO,
+    val controllerComponents: ControllerComponents
+)(implicit
     ec: ExecutionContext
 ) extends BaseController {
 
   def test: Action[AnyContent] =
     Action {
       val reportCreator = ReportCreator()
+      // TODO: Use proper values here
       val allEntries = Await.result(accountingEntryDAO.dao.all, Duration.Inf)
+      val accountingYear = 2020
+      val company = Await.result(companyDAO.dao.find(CompanyKey(1)), Duration.Inf).get
       val dataContent = StreamConverters.fromInputStream(() =>
-        new ByteArrayInputStream(reportCreator.createPdf(TestMe.mkReport(allEntries)).toByteArray)
+        new ByteArrayInputStream(reportCreator.createPdf(TestMe.mkReport(company, accountingYear, allEntries)).toByteArray)
       )
       Ok.streamed(
         dataContent,
