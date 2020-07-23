975de2ebe1c5555eb649d50be93c14381ba6921b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-07-23 18:13:37 +0200

    Add further experiments to pdf creation

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -1,25 +1,35 @@
 package controllers
+
 import java.io.ByteArrayInputStream
 
 import akka.stream.scaladsl.StreamConverters
+import db.AccountingEntryDAO
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import report.{ReportCreator, TestMe}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
 
-class ReportController @Inject() (val controllerComponents: ControllerComponents)(implicit
-                                                                                  ec: ExecutionContext
+class ReportController @Inject() (accountingEntryDAO: AccountingEntryDAO,
+                                  val controllerComponents: ControllerComponents)(implicit
+    ec: ExecutionContext
 ) extends BaseController {
 
-  def test: Action[AnyContent] = Action {
-    val reportCreator = ReportCreator()
-    val dataContent = StreamConverters.fromInputStream(() => new ByteArrayInputStream(reportCreator.createPdf(TestMe.xml).toByteArray))
-    Ok.chunked(
-      dataContent
-    )
+  def test: Action[AnyContent] =
+    Action {
+      val reportCreator = ReportCreator()
+      val allEntries = Await.result(accountingEntryDAO.dao.all, Duration.Inf)
+      val dataContent = StreamConverters.fromInputStream(() =>
+        new ByteArrayInputStream(reportCreator.createPdf(TestMe.mkReport(allEntries)).toByteArray)
+      )
+      Ok.streamed(
+        dataContent,
+        contentLength = None,
+        inline = false,
+        fileName = Some("test.pdf")
+      ).withHeaders(("Content-Type", "pdf"))
 
-  }
-
+    }
 
 }
