f1c009e276af2519b88c9b44ad580532d7edda86
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-07-23 22:15:06 +0200

    Set up more information for the XML containing report information

--- app/report/ReportCreator.scala
+++ app/report/ReportCreator.scala
@@ -1,13 +1,13 @@
 package report
 
-import java.io.{ ByteArrayOutputStream, StringReader }
+import java.io.{ByteArrayOutputStream, StringReader}
 
 import better.files._
-import db.AccountingEntry
+import db.{AccountingEntry, Company}
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.sax.SAXResult
 import javax.xml.transform.stream.StreamSource
-import org.apache.fop.apps.{ FopFactory, MimeConstants }
+import org.apache.fop.apps.{FopFactory, MimeConstants}
 
 import scala.xml.Elem
 
@@ -44,14 +44,15 @@ object TestMe {
     </report>
   }
 
-  def mkReport(accountingEntries: Seq[AccountingEntry]): Elem =
+  def mkReport(company: Company, accountingYear: Int, accountingEntries: Seq[AccountingEntry]): Elem =
     <report
-    name="Anja"
-    description="best friend"
-    complany="Bisinez">
+      name="Tabular report"
+      accountingYear={accountingYear.toString}>
+      {mkCompanyData(company)}
       {accountingEntries.map(mkAccountingEntryData)}
     </report>
 
+  // TODO: Extract amount computation - there is probably a function that can do this already.
   def mkAccountingEntryData(accountingEntry: AccountingEntry): Elem =
     <accountingEntry
       number={accountingEntry.id.toString}
@@ -60,6 +61,17 @@ object TestMe {
       debit={accountingEntry.debit.toString}
       credit={accountingEntry.credit.toString}
       amount={(accountingEntry.amountWhole + 0.01 * accountingEntry.amountChange).toString}
+      />
+
+  def mkCompanyData(company: Company): Elem =
+    <company
+      name={company.name}
+      address={company.address}
+      taxNumber={company.taxNumber}
+      revenueOffice={company.revenueOffice}
+      postalCode={company.postalCode}
+      city={company.city}
+      country={company.country}
       />
 
   def main(args: Array[String]): Unit = {
