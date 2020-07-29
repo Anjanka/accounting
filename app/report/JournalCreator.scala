64c0f2ed0f6744399329cae4ffba0e001b4d8e15
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-07-29 15:19:32 +0200

    #1 rename object reportCreator to journalCreator, add object NominalAccountsCreator, shift both to separate object files

---
+++ app/report/JournalCreator.scala
@@ -0,0 +1,50 @@
+package report
+
+import java.sql.Date
+
+import base.MonetaryValue
+import db.{AccountingEntry, Company}
+
+import scala.xml.Elem
+
+object JournalCreator {
+
+  def mkJournal(company: Company, accountingYear: Int, accountingEntries: Seq[AccountingEntry]): Elem =
+  <journal
+  pageName="Journal"
+  accountingYear={accountingYear.toString}>
+  {mkCompanyData(company)}
+  {accountingEntries.map(mkAccountingEntryData)}
+  </journal>
+
+  def mkAccountingEntryData(accountingEntry: AccountingEntry): Elem =
+  <accountingEntry
+  number={accountingEntry.id.toString}
+  date={showDate(accountingEntry.bookingDate)}
+  receiptNumber={accountingEntry.receiptNumber}
+  description={accountingEntry.description}
+  debit={accountingEntry.debit.toString}
+  credit={accountingEntry.credit.toString}
+  amount={MonetaryValue.show(MonetaryValue.fromAllCents(100 * accountingEntry.amountWhole + accountingEntry.amountChange))}
+  />
+
+  def mkCompanyData(company: Company): Elem =
+  <company
+  name={company.name}
+  address={company.address}
+  taxNumber={company.taxNumber}
+  revenueOffice={company.revenueOffice}
+  postalCode={company.postalCode}
+  city={company.city}
+  country={company.country}
+  />
+
+  def showDate(date: Date): String = {
+  val localDate = date.toLocalDate
+  def pad(dateComponent: Int): String = if (dateComponent < 10) s"0$dateComponent" else dateComponent.toString
+  List(pad(localDate.getDayOfMonth), pad(localDate.getMonthValue), localDate.getYear).mkString(".")
+
+}
+
+
+}
--- app/report/ReportCreator.scala
+++ app/report/ReportCreator.scala
@@ -14,16 +14,25 @@ import org.apache.fop.apps.{FopFactory, MimeConstants}
 import scala.xml.Elem
 
 case class ReportCreator() {
-  private val xsltStyleFile = "conf" / "xslt" / "journal.xsl"
+  private val journalStyleFile = "conf" / "xslt" / "journal.xsl"
+  private val nominalAccountsStyleFile = "conf" / "xslt" / "nominalAccounts.xsl"
   private val fopFactory = FopFactory.newInstance()
   private val foUserAgent = fopFactory.newFOUserAgent()
 
-  def createPdf(xml: Elem): ByteArrayOutputStream = {
+  def createJournalPdf(xml: Elem): ByteArrayOutputStream = {
+    createPdfWith(journalStyleFile)(xml)
+  }
+
+  def createNominalAccountsPdf(xml: Elem): ByteArrayOutputStream = {
+    createPdfWith(nominalAccountsStyleFile)(xml)
+  }
+
+  private def createPdfWith(xsltFile: File)(xml: Elem): ByteArrayOutputStream = {
     val outputStream = new ByteArrayOutputStream()
     val fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, outputStream)
 
     val factory = TransformerFactory.newInstance()
-    val transformer = factory.newTransformer(new StreamSource(xsltStyleFile.toJava))
+    val transformer = factory.newTransformer(new StreamSource(xsltFile.toJava))
 
     val src = new StreamSource(new StringReader(xml.toString()))
     val res = new SAXResult(fop.getDefaultHandler)
@@ -35,59 +44,5 @@ case class ReportCreator() {
 
 }
 
-object TestMe {
-
-  val xml: Elem = {
-    <report
-      name="Anja"
-      description="best friend"
-      complany="Bisinez">
-      {}
-    </report>
-  }
-
-  def mkReport(company: Company, accountingYear: Int, accountingEntries: Seq[AccountingEntry]): Elem =
-    <report
-      pageName="Journal"
-      accountingYear={accountingYear.toString}>
-      {mkCompanyData(company)}
-      {accountingEntries.map(mkAccountingEntryData)}
-    </report>
-
-  // TODO: Extract amount computation - there is probably a function that can do this already.
-  def mkAccountingEntryData(accountingEntry: AccountingEntry): Elem =
-    <accountingEntry
-      number={accountingEntry.id.toString}
-      date={showDate(accountingEntry.bookingDate)}
-      receiptNumber={accountingEntry.receiptNumber}
-      description={accountingEntry.description}
-      debit={accountingEntry.debit.toString}
-      credit={accountingEntry.credit.toString}
-      amount={MonetaryValue.show(MonetaryValue.fromAllCents(100 * accountingEntry.amountWhole + accountingEntry.amountChange))}
-      />
 
 
-  def mkCompanyData(company: Company): Elem =
-    <company
-      name={company.name}
-      address={company.address}
-      taxNumber={company.taxNumber}
-      revenueOffice={company.revenueOffice}
-      postalCode={company.postalCode}
-      city={company.city}
-      country={company.country}
-      />
-
-  def showDate(date: Date): String = {
-    val localDate = date.toLocalDate
-    def pad(dateComponent: Int): String = if (dateComponent < 10) s"0$dateComponent" else dateComponent.toString
-    List(pad(localDate.getDayOfMonth), pad(localDate.getMonthValue), localDate.getYear).mkString(".")
-
-  }
-
-  def main(args: Array[String]): Unit = {
-    val reportCreator = ReportCreator()
-    reportCreator.createPdf(xml)
-  }
-
-}
