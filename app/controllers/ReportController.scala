5d5228b2571ce1f278d9e0238a7d3fb80bb95e33
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-07-29 15:21:10 +0200

    #1 adjust to renaming of JournalCreator, start work on nominalAccounts function

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -8,7 +8,7 @@ import db.AccountingEntryDAO.CompanyYearKey
 import db.{AccountingEntryDAO, CompanyDAO}
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
-import report.{ReportCreator, TestMe}
+import report.{JournalCreator, NominalAccountsCreator, ReportCreator}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
@@ -28,13 +28,31 @@ class ReportController @Inject() (
       val allEntries = Await.result(accountingEntryDAO.dao.findPartial(CompanyYearKey(companyId, accountingYear))(AccountingEntryDAO.compareCompanyYearKey), Duration.Inf)
       val company = Await.result(companyDAO.dao.find(CompanyKey(companyId)), Duration.Inf).get
       val dataContent = StreamConverters.fromInputStream(() =>
-        new ByteArrayInputStream(reportCreator.createPdf(TestMe.mkReport(company, accountingYear, allEntries)).toByteArray)
+        new ByteArrayInputStream(reportCreator.createJournalPdf(JournalCreator.mkJournal(company, accountingYear, allEntries)).toByteArray)
       )
       Ok.streamed(
         dataContent,
         contentLength = None,
         inline = false,
         fileName = Some(s"journal $accountingYear.pdf")
+      ).withHeaders(("Content-Type", "pdf"))
+
+    }
+
+  def nominalAccounts(companyId: Int, accountingYear: Int): Action[AnyContent] =
+    Action {
+      val reportCreator = ReportCreator()
+      // TODO: Use proper values here
+      val allEntries = Await.result(accountingEntryDAO.dao.findPartial(CompanyYearKey(companyId, accountingYear))(AccountingEntryDAO.compareCompanyYearKey), Duration.Inf)
+      val company = Await.result(companyDAO.dao.find(CompanyKey(companyId)), Duration.Inf).get
+      val dataContent = StreamConverters.fromInputStream(() =>
+        new ByteArrayInputStream(reportCreator.createNominalAccountsPdf(NominalAccountsCreator.mkNominalAccounts(company, accountingYear, allEntries)).toByteArray)
+      )
+      Ok.streamed(
+        dataContent,
+        contentLength = None,
+        inline = false,
+        fileName = Some(s"nominalAccounts $accountingYear.pdf")
       ).withHeaders(("Content-Type", "pdf"))
 
     }
--- conf/xslt/journal.xsl
+++ conf/xslt/journal.xsl
@@ -1,7 +1,7 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:fo="http://www.w3.org/1999/XSL/Format">
 
-    <xsl:template match="report">
+    <xsl:template match="journal">
         <fo:root>
             <fo:layout-master-set>
                 <fo:simple-page-master master-name="simpleA4"
