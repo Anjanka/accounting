36f334238865b9c7d073c5d511b83f01d6d1542a
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-07-30 12:06:08 +0200

    #1 sort nominal accounts by Account id and entries by booking date and receipt number

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -1,17 +1,17 @@
 package controllers
 
 import java.io.ByteArrayInputStream
+import java.sql.Date
 
-import abstraction.NominalAccountEntry.{Credit, Debit}
 import abstraction.{NominalAccount, NominalAccountEntry}
 import akka.stream.scaladsl.StreamConverters
 import base.Id.CompanyKey
-import base.MonetaryValue
 import db.AccountingEntryDAO.CompanyYearKey
-import db.{Account, AccountDAO, AccountingEntry, AccountingEntryDAO, CompanyDAO}
+import db._
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import report.{JournalCreator, NominalAccountsCreator, ReportCreator}
+import Ordering.Implicits._
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
@@ -66,7 +66,9 @@ class ReportController @Inject() (
       val dataContent = StreamConverters.fromInputStream(() =>
         new ByteArrayInputStream(
           reportCreator
-            .createNominalAccountsPdf(NominalAccountsCreator.mkNominalAccounts(company, accountingYear, allNominalAccountEntries))
+            .createNominalAccountsPdf(
+              NominalAccountsCreator.mkNominalAccounts(company, accountingYear, allNominalAccountEntries)
+            )
             .toByteArray
         )
       )
@@ -79,13 +81,53 @@ class ReportController @Inject() (
 
     }
 
-  def getNominalAccounts(entries: Seq[AccountingEntry], accounts : Seq[Account]): Seq[NominalAccount] = {
-    entries
-      .flatMap(entry => List(entry.credit -> NominalAccountEntry.mkCreditEntry(entry), entry.debit -> NominalAccountEntry.mkDebitEntry(entry)))
-      .groupBy(pair => pair._1)
-      .map { case (i, tuples) => NominalAccount(i, accounts.filter(i == _.id).head.title, tuples.map(pair => pair._2)) }
-      .toSeq
+
+
+  case class DateReceiptNumberPair(date: Date, receiptNumber: String)
+
+  implicit val date_receiptNumberOrdering: Ordering[DateReceiptNumberPair] = (x: DateReceiptNumberPair, y: DateReceiptNumberPair) => {
+    if (x.date < y.date) -1
+    else if (x.date > y.date) 1
+    else if (x.receiptNumber < y.receiptNumber) -1
+    else if (x.receiptNumber > y.receiptNumber) 1
+    else 0
   }
 
+  implicit val dateOrdering: Ordering[Date] = (x: Date, y: Date) => {
+    val localX = x.toLocalDate
+    val localY = y.toLocalDate
+    if (localX.getYear < localY.getYear) -1
+    else if (localX.getYear > localY.getYear) 1
+    else if (localX.getMonthValue < localY.getMonthValue) -1
+    else if (localX.getMonthValue > localY.getMonthValue) 1
+    else if (localX.getDayOfMonth < localY.getDayOfMonth) -1
+    else if (localX.getDayOfMonth > localY.getDayOfMonth) 1
+    else 0
+  }
+
+  def getNominalAccounts(entries: Seq[AccountingEntry], accounts: Seq[Account]): Seq[NominalAccount] = {
+    entries
+      .flatMap(entry =>
+        List(
+          entry.credit -> NominalAccountEntry.mkCreditEntry(entry),
+          entry.debit -> NominalAccountEntry.mkDebitEntry(entry)
+        )
+      )
+      .groupBy(pair => pair._1)
+      .map {
+        case (i, tuples) =>
+          NominalAccount(
+            i,
+            accounts.filter(i == _.id).head.title,
+            tuples
+              .map(pair => pair._2)
+              .sortBy(entry => DateReceiptNumberPair(entry.bookingDate, entry.receiptNumber))(
+                date_receiptNumberOrdering
+              )
+          )
+      }
+      .toSeq
+      .sortBy(_.accountId)
+  }
 
 }
