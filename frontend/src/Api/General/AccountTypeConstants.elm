c15844d1f76743d3288b44ea8c2ddf047633118b
Anja Hildebrandt <anja.hildebrandt@gmx.de>
2020-08-25 14:37:17 +0200

    #5 remodel account type

--- frontend/src/Api/General/AccountTypeConstants.elm
+++ frontend/src/Api/General/AccountTypeConstants.elm
@@ -3,43 +3,84 @@ module Api.General.AccountTypeConstants exposing (..)
 import Api.General.AccountUtil exposing (AccountType)
 
 
-englishAccountTypes : List AccountType
+type alias AccountTypes =
+    { inferiorAssets : String
+    , cashAccount : String
+    , purchasedGoods : String
+    , telephoneCosts : String
+    , travelExpenses : String
+    , other : String
+    , interestIncome : String
+    , openingBalance : String
+    , salesRevenue : String
+    , personnelCosts : String
+    , postalCharges : String
+    , leaseExpenses : String
+    , loans : String
+    , debts : String
+    , prepaidTax : String
+    , salesTaxes : String
+    }
+
+
+makeTypeList : AccountTypes -> List AccountType
+makeTypeList ats =
+    [ { id = 11, categoryIds = [ 1 ], name = ats.inferiorAssets }
+    , { id = 1, categoryIds = [ 0 ], name = ats.cashAccount }
+    , { id = 31, categoryIds = [ 3 ], name = ats.purchasedGoods }
+    , { id = 32, categoryIds = [ 3 ], name = ats.telephoneCosts }
+    , { id = 33, categoryIds = [ 3 ], name = ats.travelExpenses }
+    , { id = 0, categoryIds = [ 7, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ], name = ats.other }
+    , { id = 81, categoryIds = [ 8 ], name = ats.interestIncome }
+    , { id = 91, categoryIds = [ 9 ], name = ats.openingBalance }
+    , { id = 82, categoryIds = [ 8 ], name = ats.salesRevenue }
+    , { id = 34, categoryIds = [ 3 ], name = ats.personnelCosts }
+    , { id = 35, categoryIds = [ 3 ], name = ats.postalCharges }
+    , { id = 36, categoryIds = [ 3 ], name = ats.leaseExpenses }
+    , { id = 41, categoryIds = [ 4 ], name = ats.loans }
+    , { id = 42, categoryIds = [ 4 ], name = ats.debts }
+    , { id = 51, categoryIds = [ 5 ], name = ats.prepaidTax }
+    , { id = 52, categoryIds = [ 5 ], name = ats.salesTaxes }
+    ]
+
+
+englishAccountTypes : AccountTypes
 englishAccountTypes =
-    [ { id = 11, categoryIds = [ 1 ], name = "inferior assets" }
-    , { id = 1, categoryIds = [ 0 ], name = "cash account" }
-    , { id = 31, categoryIds = [ 3 ], name = "purchased goods" }
-    , { id = 32, categoryIds = [ 3 ], name = "telephone costs" }
-    , { id = 33, categoryIds = [ 3 ], name = "travel expenses" }
-    , { id = 0, categoryIds = [ 7, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ], name = "other" }
-    , { id = 81, categoryIds = [ 8 ], name = "interest income" }
-    , { id = 91, categoryIds = [ 9 ], name = "opening balance" }
-    , { id = 82, categoryIds = [ 8 ], name = "sales revenue" }
-    , { id = 34, categoryIds = [ 3 ], name = "personnel costs" }
-    , { id = 35, categoryIds = [ 3 ], name = "postal charges" }
-    , { id = 36, categoryIds = [ 3 ], name = "lease expenses" }
-    , { id = 41, categoryIds = [ 4 ], name = "loans" }
-    , { id = 42, categoryIds = [ 4 ], name = "debts" }
-    , { id = 51, categoryIds = [ 5 ], name = "prepaid tax" }
-    , { id = 52, categoryIds = [ 5 ], name = "sales taxes" }
-    ]
+    { inferiorAssets = "inferior assets"
+    , cashAccount = "cash account"
+    , purchasedGoods = "purchased goods"
+    , telephoneCosts = "telephone costs"
+    , travelExpenses = "travel expenses"
+    , other = "other"
+    , interestIncome = "interest income"
+    , openingBalance = "opening balance"
+    , salesRevenue = "sales revenue"
+    , personnelCosts = "personnel costs"
+    , postalCharges = "postal charges"
+    , leaseExpenses = "lease expenses"
+    , loans = "loans"
+    , debts = "debts"
+    , prepaidTax = "prepaid tax"
+    , salesTaxes = "sales taxes"
+    }
 
 
-germanAccountTypes : List AccountType
+germanAccountTypes : AccountTypes
 germanAccountTypes =
-    [ { id = 11, categoryIds = [ 1 ], name = "Geringwertige WG" }
-    , { id = 1, categoryIds = [ 0 ], name = "Kassenkonto" }
-    , { id = 31, categoryIds = [ 3 ], name = "Wareneinkauf" }
-    , { id = 32, categoryIds = [ 3 ], name = "Telefonkosten" }
-    , { id = 33, categoryIds = [ 3 ], name = "Reisekosten" }
-    , { id = 0, categoryIds = [ 7, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ], name = "Sonstige" }
-    , { id = 81, categoryIds = [ 8 ], name = "Zinserträge" }
-    , { id = 91, categoryIds = [ 9 ], name = "Eröffungsbilanz" }
-    , { id = 82, categoryIds = [ 8 ], name = "Umsatzerlöse" }
-    , { id = 34, categoryIds = [ 3 ], name = "Personalkosten" }
-    , { id = 35, categoryIds = [ 3 ], name = "Portokosten" }
-    , { id = 36, categoryIds = [ 3 ], name = "Miete" }
-    , { id = 41, categoryIds = [ 4 ], name = "Darlehen" }
-    , { id = 42, categoryIds = [ 4 ], name = "Verbindlichkeiten" }
-    , { id = 51, categoryIds = [ 5 ], name = "Vorsteuer" }
-    , { id = 52, categoryIds = [ 5 ], name = "Umsatzsteuer" }
-    ]
+    { inferiorAssets = "Geringwertige WG"
+    , cashAccount = "Kassenkonto"
+    , purchasedGoods = "Wareneinkauf"
+    , telephoneCosts = "Telefonkosten"
+    , travelExpenses = "Reisekosten"
+    , other = "Sonstige"
+    , interestIncome = "Zinserträge"
+    , openingBalance = "Eröffungsbilanz"
+    , salesRevenue = "Umsatzerlöse"
+    , personnelCosts = "Personalkosten"
+    , postalCharges = "Portokosten"
+    , leaseExpenses = "Miete"
+    , loans = "Darlehen"
+    , debts = "Verbindlichkeiten"
+    , prepaidTax = "Vorsteuer"
+    , salesTaxes = "Umsatzsteuer"
+    }
