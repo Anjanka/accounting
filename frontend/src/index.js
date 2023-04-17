4a69bd22d5c4d1eb7a8d6e524b1ddb95dd69baa8
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-17 22:21:56 +0200

    Add central entry point, and ports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,21 @@ import {Elm} from './Main.elm'
 import './style.css'
 
 const app = Elm.Main.init({
-    node: document.getElementById('root')
+    node: document.getElementById('root'),
+    flags: {
+        backendURL: process.env.ELM_APP_BACKEND_URL
+    }
+})
+
+const tokenKey = 'accounting-user-token'
+
+app.ports.storeToken.subscribe(function (token) {
+    localStorage.setItem(tokenKey, token)
+    app.ports.fetchToken.send(token)
+})
+
+app.ports.doFetchToken.subscribe(function () {
+    const storedToken = localStorage.getItem(tokenKey)
+    const tokenOrEmpty = storedToken ? storedToken : ''
+    app.ports.fetchToken.send(tokenOrEmpty)
 })
