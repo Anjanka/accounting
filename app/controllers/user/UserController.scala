659381437f2487f51716e517c7b26d12c2af0e8f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-18 10:03:38 +0200

    Add login route

--- app/controllers/user/UserController.scala
+++ app/controllers/user/UserController.scala
@@ -1,15 +1,49 @@
 package controllers.user
 
+import io.circe.syntax._
 import play.api.libs.circe.Circe
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{ AbstractController, Action, ControllerComponents }
+import security.jwt.{ JwtConfiguration, JwtExpiration, LoginContent }
+import user.Credentials
+import utils.jwt.JwtUtil
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.util.chaining._
 
 class UserController @Inject() (
-    cc: ControllerComponents,
-    userConfiguration: UserConfiguration
-)(implicit
-    executionContext: ExecutionContext
+    cc: ControllerComponents
 ) extends AbstractController(cc)
-    with Circe
+    with Circe {
+
+  private val jwtConfiguration = JwtConfiguration.default
+
+  private val userConfiguration = UserConfiguration.default
+
+  def login: Action[Credentials] =
+    Action(circe.tolerantJson[Credentials]) { request =>
+      val credentials = request.body
+      val jwtCandidate =
+        if (UserConfiguration.validateCredentials(credentials, userConfiguration)) {
+          val jwt = JwtUtil.createJwt(
+            content = LoginContent(
+              userId = userConfiguration.id
+            ),
+            privateKey = jwtConfiguration.signaturePrivateKey,
+            expiration = JwtExpiration.Expiring(
+              start = System.currentTimeMillis() / 1000,
+              duration = jwtConfiguration.restrictedDurationInSeconds
+            )
+          )
+          Some(jwt)
+        } else None
+
+      jwtCandidate
+        .fold(
+          BadRequest("Invalid credentials")
+        )(
+          _.pipe(_.asJson)
+            .pipe(Ok(_))
+        )
+    }
+
+}
